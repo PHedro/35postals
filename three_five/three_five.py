f6200417be92ee4c6ddbd6c5dbd049a78846cd01
PHedro <pedrohenrique.reis@gmail.com>
2020-07-25 00:38:19 +0100

    remove printer function based on flavor to reduce  the complexity when we want to print the results;  Added default parameter print_output=True to print  the values.  Default True as the requirement is to print the  values.

--- three_five/three_five.py
+++ three_five/three_five.py
@@ -3,7 +3,7 @@ FIVE_STR = "Five"
 THREE_FIVE_STR = "ThreeFive"
 
 
-def three_five(lower=1, upper=101):
+def three_five(lower=1, upper=101, print_output=True):
     result = []
 
     # we need to remember that the upper
@@ -20,12 +20,14 @@ def three_five(lower=1, upper=101):
         else:
             _value = _num
 
+        if print_output:
+            print(_value)
         result.append(_value)
 
     return result
 
 
-def three_five_generator(lower=1, upper=101):
+def three_five_generator(lower=1, upper=101, print_output=True):
     for _num in range(lower, upper):
         five, three, three_and_five = _is_three_and_five_multiple(_num)
 
@@ -37,6 +39,9 @@ def three_five_generator(lower=1, upper=101):
             _value = THREE_STR
         else:
             _value = _num
+
+        if print_output:
+            print(_value)
         yield _value
 
 
@@ -47,8 +52,3 @@ def _is_three_and_five_multiple(_num):
         five = not _num % 5
         three_and_five = three and five
     return five, three, three_and_five
-
-
-def print_three_five(flavor=three_five, lower=1, upper=101):
-    for _item in flavor(lower=lower, upper=upper):
-        print(_item)
